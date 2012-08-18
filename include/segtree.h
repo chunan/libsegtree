2a1735c45e70983a07fa1d39342a86ba31f45a51
ugoc <ugocugoc@gmail.com>
2012-08-18 12:12:03 +0800

    array->vector, Complete ConstructTree()
    UpdateLossStat() separated from Load_segtree()

--- include/segtree.h
+++ include/segtree.h
@@ -3,19 +3,35 @@
 
 #include "ugoc_utility.h"
 #include "feature.h"
+#include <boost/heap/fibonacci_heap.hpp>
+
+using boost::heap::fibonacci_heap;
+using boost::heap::compare;
+
+struct MergeOp {
+
+  struct GreaterLoss : binary_function <MergeOp, MergeOp, bool> {
+    bool operator() (const MergeOp& a, const MergeOp& b) const {
+      return a.loss > b.loss;
+    }
+  };
+
+  int lchild, rchild;
+  float loss;
+  float lm2; /* (length * ||mean|| ^ 2) of newly created segment */
+  fibonacci_heap<MergeOp, compare<GreaterLoss> >::handle_type lmerge, rmerge;
+};
+
 
 class SegTree{
   public:
     SegTree() { Init(); }
     SegTree(char * const fname) { Load_segtree(fname); }
-    ~SegTree(){ Free(); }
     void Init();
-    void Free();
     void Load_segtree(string fname);
     void Save_segtree(string fname);
     void GetBasicSeg(vector<int>* index, float threshold);
-    // TODO: not implemented yet
-    void ConstructTree(const Feature& feat);
+    void ConstructTree(const DenseFeature& feat);
     /* accessors */
     int StartT(int seg_idx) const;
     int EndT(int seg_idx) const;
@@ -31,12 +47,14 @@ class SegTree{
 
   private:
     void Reallocate(int n_node);
+    void AssignSeg(const MergeOp& op, int t, vector<float>& lm2);
+    void UpdateLossStat();
     // Data
-    int *start_t;    // start_t[non_leaf]
-    int *end_t;      // end_t[non_leaf]
-    int *parent;     // parent[num_node]
-    int **child;     // child[non_leaf]
-    float *merge_loss; // merge_loss[non_leaf]
+    vector<int32_t> start_t;    // start_t[non_leaf]
+    vector<int32_t> end_t;      // end_t[non_leaf]
+    vector<int32_t> parent;     // parent[num_node]
+    vector<int32_t> child;      // child[2 * non_leaf]
+    vector<float> merge_loss;   // merge_loss[non_leaf]
 
     float merge_std;
     float merge_mean;
@@ -44,21 +62,6 @@ class SegTree{
     int num_node;
     int non_leaf;
     int num_leaf; // number of frames
-
-    /*
-     * node_size:
-     *   record the size of start_t, end_t, parent
-     * non_leaf_size, num_child, child_array_size:
-     *   record the child 2d array size
-     *   (non_leaf_size * num_child)  <= child_array_size
-     *
-     * They are used to record actural memory size because SegTree
-     * allows to be reloaded. See SegTree::Load_segtree() for detail.
-     */
-    int node_size;
-    int non_leaf_size;
-    int num_child; // 2
-    int child_array_size;
 
 };
 #endif
--- src/segtree.cpp
+++ src/segtree.cpp
@@ -3,92 +3,69 @@
 #include <cassert>
 #include <list>
 #include <cmath>
+#include <numeric>
+#include <functional>
 #include "segtree.h"
 
-const int sizeInt = sizeof(int);
 const int sizeFloat = sizeof(float);
 
 void SegTree::Init() {/*{{{*/
-  start_t   = NULL;
-  end_t     = NULL;
-  parent    = NULL;
-  child     = NULL;
-  merge_loss  = NULL;
   merge_mean = 0.0;
   merge_std = 0.0;
   num_node  = 0;
-  node_size  = 0;
   non_leaf  = 0;
   num_leaf  = 0;
-  num_child = 0;
-  child_array_size = 0;
-}/*}}}*/
-
-void SegTree::Free() {/*{{{*/
-  if (Free_1d_array(start_t)) {
-    end_t = NULL;
-    parent = NULL;
-  }
-  mem_op<int>::delete_2d_array(&child);
-  Free_1d_array(merge_loss);
-  merge_mean = 0.0;
-  merge_std = 0.0;
-  node_size  = 0;
-  non_leaf  = 0;
-  num_leaf  = 0;
-  num_child = 0;
-  child_array_size = 0;
 }/*}}}*/
 
 void SegTree::Reallocate(int n_node) {/*{{{*/
+
   num_node = n_node;
   num_leaf = (num_node + 1) / 2;
   non_leaf = num_node - num_leaf;
-  mem_op<int>::reallocate_2d_array(&child, non_leaf, 2,
-                                   &non_leaf_size,
-                                   &num_child,
-                                   &child_array_size);
 
-  if (num_node > node_size) { /* Reallocat Mem */
-    /* start_t, end_t, parents */
-    Free_1d_array(start_t);
-    start_t = new int [2 * non_leaf + num_node];
-    /* merge loss */
-    Free_1d_array(merge_loss);
-    merge_loss = new float[non_leaf];
-    node_size = num_node;
-  }
-  end_t = start_t + non_leaf;
-  parent = end_t + non_leaf;
+  start_t.resize(non_leaf);
+  end_t.resize(non_leaf);
+  parent.resize(num_node);
+  child.resize(2 * non_leaf);
+  merge_loss.resize(non_leaf);
+
 }/*}}}*/
 
 void SegTree::Load_segtree(string fname) {/*{{{*/
-  FILE *fd = FOPEN(fname.c_str(),"r");
-  size_t ret;
-  int n_node;
+
+  FILE *fd = FOPEN(fname.c_str(), "r");
+  int ret;
+  int32_t n_node;
   /* num_node, non_leaf, num_leaf, child */
-  ret = fread(&n_node,sizeInt,1,fd);
+  ret = fread(&n_node, 4, 1, fd);
   assert(ret == 1);
+
   Reallocate(n_node);
 
-  ret = fread(start_t, sizeInt, non_leaf, fd);
-  assert(ret == static_cast<unsigned>(non_leaf));
-  ret = fread(end_t, sizeInt, non_leaf, fd);
-  assert(ret == static_cast<unsigned>(non_leaf));
-  ret = fread(parent, sizeInt, num_node, fd);
-  assert(ret == static_cast<unsigned>(num_node));
-  ret = fread(child[0], sizeInt, 2 * non_leaf, fd);
-  assert(ret == 2 * static_cast<unsigned>(non_leaf));
-  ret = fread(merge_loss, sizeFloat, non_leaf, fd);
-  assert(ret == static_cast<unsigned>(non_leaf));
-  int bseg_th;
-  ret = fread(&bseg_th, sizeFloat, 1, fd);
-  assert(ret == 1);
-  ret = fread(&merge_std, sizeFloat, 1, fd);
-  assert(ret == 1);
+  ret = fread(&start_t[0], 4, non_leaf, fd);
+  assert(ret == non_leaf);
+
+  ret = fread(&end_t[0], 4, non_leaf, fd);
+  assert(ret == non_leaf);
+
+  ret = fread(&parent[0], 4, num_node, fd);
+  assert(ret == num_node);
+
+  ret = fread(&child[0], 4, 2 * non_leaf, fd);
+  assert(ret == 2 * non_leaf);
+
+  ret = fread(&merge_loss[0], sizeFloat, non_leaf, fd);
+  assert(ret == non_leaf);
+
   fclose(fd);
 
   //cout << "file std = " << merge_std << endl;
+
+  UpdateLossStat();
+
+}/*}}}*/
+
+void SegTree::UpdateLossStat() {/*{{{*/
 
   /* Calculate Mean & Std */
   double sum = 0.0;
@@ -100,6 +77,7 @@ void SegTree::Load_segtree(string fname) {/*{{{*/
   merge_mean = sum / non_leaf;
   merge_std = sqrt(sqr_sum / non_leaf - merge_mean * merge_mean);
 
+  /* Eliminate too large mergings */
   int n_merge = non_leaf;
   double cutoff = merge_mean + 10.0 * merge_std;
   for (int i = non_leaf - 1; i >= 0; --i) {
@@ -112,16 +90,146 @@ void SegTree::Load_segtree(string fname) {/*{{{*/
   merge_mean = sum / n_merge;
   merge_std = sqrt(sqr_sum / n_merge - merge_mean * merge_mean);
 
-  //cout << "new std  = " << merge_std << endl;
+}/*}}}*/
 
+
+void SegTree::Save_segtree(string fname) {/*{{{*/
+
+  FILE *fd = FOPEN(fname.c_str(), "w");
+  int ret;
+  int32_t n_node;
+  /* num_node, non_leaf, num_leaf, child */
+  ret = fread(&n_node, 4, 1, fd);
+  assert(ret == 1);
+
+  ret = fwrite(&start_t[0], 4, non_leaf, fd);
+  assert(ret == non_leaf);
+
+  ret = fwrite(&end_t[0], 4, non_leaf, fd);
+  assert(ret == non_leaf);
+
+  ret = fwrite(&parent[0], 4, num_node, fd);
+  assert(ret == num_node);
+
+  ret = fwrite(&child[0], 4, 2 * non_leaf, fd);
+  assert(ret == 2 * non_leaf);
+
+  ret = fwrite(&merge_loss[0], sizeFloat, non_leaf, fd);
+  assert(ret == non_leaf);
+
+  fclose(fd);
 
 }/*}}}*/
 
-void SegTree::ConstructTree(const Feature& feat) {
+
+
+typedef fibonacci_heap<MergeOp, compare<MergeOp::GreaterLoss> > MergeOpHeap_t;
+
+float AddThenSquare(float a, float b) { return (a + b) * (a + b); }
+
+float SubThenSquare(float a, float b) { return (a - b) * (a - b); }
+
+float CalLm2(TwoDimVector<float>& accum_feat, int s, int e) {/*{{{*/
+  float len = e - s + 1;
+  vector<float> v = accum_feat[e];
+
+  if (s > 0) {
+    for (unsigned i =0; i < v.size(); ++i) {
+      v[i] -= accum_feat(s - 1, i);
+    }
+  }
+  return inner_product(v.begin(), v.end(), v.begin(), 0.0f) / len;
+}/*}}}*/
+
+void SegTree::AssignSeg(const MergeOp& op, int t, vector<float>& lm2) {/*{{{*/
+
+  int seg_idx = num_leaf + t;
+
+  start_t[t] = StartT(op.lchild);
+  end_t[t] = EndT(op.rchild);
+  parent[op.lchild] = seg_idx;
+  parent[op.rchild] = seg_idx;
+  child[2 * t] = op.lchild;
+  child[2 * t + 1] = op.rchild;
+  lm2[seg_idx] = op.lm2;
+  merge_loss[t] = max<float>(MergeLoss(seg_idx - 1), op.loss);
+
+}/*}}}*/
+
+void SegTree::ConstructTree(const DenseFeature& feat) {/*{{{*/
+
   Reallocate(2 * feat.LT() - 1);
-}
+
+  /* Calculate (-length * ||mean|| ^ 2) for each frame */
+  vector<float> lm2(num_node - 1);
+  for (int t = 0; t < num_leaf; ++t) {
+    lm2[t] = -inner_product(feat.Data()[t].begin(), feat.Data()[t].end(),
+                            feat.Data()[t].begin(), 0.0f);
+  }
+
+  /* Calculate initial segments and push into heap */
+  MergeOpHeap_t heap;
+  MergeOpHeap_t::handle_type prev_handle, this_handle;
+  for (int t = 0; t < non_leaf; ++t) {
+    MergeOp op;
+    op.lchild = t;
+    op.rchild = t + 1;
+    op.lm2 = -0.5 * inner_product(
+        feat.Data()[t].begin(), feat.Data()[t].end(),
+        feat.Data()[t + 1].begin(), 0.0f, plus<float>(), AddThenSquare);
+    op.loss = op.lm2 - lm2[t] - lm2[t + 1];
+    op.lmerge = prev_handle;
+    this_handle = heap.push(op);
+    if (prev_handle.node_ != NULL) {
+      (*prev_handle).rmerge = this_handle;
+    }
+    prev_handle = this_handle;
+  }
+
+  /* Calculate accumulated feature so mean vector can be calculated in O(1) */
+  TwoDimVector<float> accum_feat = feat.Data();
+  for (int t = 1; t < feat.LT(); ++t) {
+    for (int i = 0; i < feat.LF(); ++i) {
+      accum_feat(t, i) += accum_feat(t - 1, i);
+    }
+  }
+
+  /* HAC */
+  for (int t = 0; t < non_leaf; ++t) {
+    const MergeOp& op = heap.top();
+    AssignSeg(op, t, lm2);
+    MergeOpHeap_t::handle_type left_hand = op.lmerge, right_hand = op.rmerge;
+
+    heap.pop();
+
+    /* Change left neighbor merge */
+    if (left_hand.node_ != NULL) {
+      MergeOp& lop = *left_hand;
+      lop.rchild = num_leaf + t;
+      lop.rmerge = right_hand;
+      lop.lm2 = -CalLm2(accum_feat, StartT(lop.lchild), EndT(lop.rchild));
+      lop.loss = lop.lm2 - lm2[lop.lchild] - lm2[lop.rchild];
+      heap.update(left_hand);
+    }
+
+    /* Change right neighbor merge */
+    if (right_hand.node_ != NULL) {
+      MergeOp& rop = *right_hand;
+      rop.lchild = num_leaf + t;
+      rop.lmerge = left_hand;
+      rop.lm2 = -CalLm2(accum_feat, StartT(rop.lchild), EndT(rop.rchild));
+      rop.loss = rop.lm2 - lm2[rop.lchild] - lm2[rop.rchild];
+      heap.update(right_hand);
+    }
+  }
+  UpdateLossStat();
+
+}/*}}}*/
+
+
 
 void SegTree::DumpData() const {/*{{{*/
+
   printf("Parent: \n");
   for (int i = 0; i < num_node; ++i) {
     printf("Parent[%3d] = %3d\n", i, Parent(i));
@@ -130,7 +238,12 @@ void SegTree::DumpData() const {/*{{{*/
   for (int i = NumLeaf(); i < NumNode(); ++i) {
     printf("Child[%3d] = {%3d, %3d}\n", i, Child(i, 0), Child(i, 1));
   }
-
+  printf("Mergeloss: \n");
+  for (int i = NumLeaf(); i < NumNode(); ++i) {
+    printf("Mergeloss[%3d] = %.4f\n", i, MergeLoss(i));
+  }
+  printf("mean = %.4f\n", merge_mean);
+  printf("std  = %.4f\n", merge_std);
 
 }/*}}}*/
 
@@ -173,7 +286,8 @@ int SegTree::Parent(int child_id) const {/*{{{*/
 
 int SegTree::Child(int parent_id, int which_child) const {/*{{{*/
   assert(parent_id >= 0 && parent_id < num_node);
-  return (parent_id < num_leaf) ? -1 : child[parent_id - num_leaf][which_child];
+  return (parent_id < num_leaf) ? -1 :
+    child[2 * (parent_id - num_leaf) + which_child];
 }/*}}}*/
 
 float SegTree::MergeLoss(int seg_idx) const {/*{{{*/
@@ -181,7 +295,9 @@ float SegTree::MergeLoss(int seg_idx) const {/*{{{*/
   return (seg_idx < num_leaf) ? 0.0 : merge_loss[seg_idx - num_leaf];
 }/*}}}*/
 
-void SegTree::GetBasicSeg(vector<int>* index, float threshold) {
+void SegTree::GetBasicSeg(vector<int>* index, float threshold) {/*{{{*/
+
+  /* Do a DFS on segtree (BFS mess up the order) */
   index->clear();
   std::list<int> nodes_to_visit;
   nodes_to_visit.push_front(num_node - 1);
@@ -192,8 +308,10 @@ void SegTree::GetBasicSeg(vector<int>* index, float threshold) {
     if (MergeLoss(visiting) <= threshold) {
       index->push_back(visiting);
     } else {
-      nodes_to_visit.push_front(child[visiting - num_leaf][1]);
-      nodes_to_visit.push_front(child[visiting - num_leaf][0]);
+      nodes_to_visit.push_front(Child(visiting, 1));
+      nodes_to_visit.push_front(Child(visiting, 0));
     }
   }
-}
+
+}/*}}}*/
+
